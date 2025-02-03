586b189fc8edd0549a1c3c1c8e648f4d9fe57a2e
hpeng04 <144637938+hpeng04@users.noreply.github.com>
2025-02-03 14:11:32 -0700

    refactor channels to objects

--- channels.py
+++ channels.py
@@ -1,43 +1,65 @@
+from rules import check_limits, check_temperature
+
+class Channel:
+    def __init__(self, name:str, min_value:float, max_value:float, regex:str, check_func:callable=None):
+        self.name = name
+        self.min_value = min_value
+        self.max_value = max_value
+        self.regex = regex
+        self.check_func = check_func
+
+    def __str__(self):
+        return f"Channel {self.name}"
+    
+    def __repr__(self):
+        return f"Channel {self.name}, {self.min_value}-{self.max_value}, {self.regex}\n"
+    
+    def check(self, data, unit_no):
+        if self.check_func:
+            self.check_func(self.regex, data, self.min_value, self.max_value, unit_no)
+        else:
+            pass
+
 channels = {
-    "A/C Watts": (0, 3000, "A/C Watts$"),
-    "AHU Watts": (0, 310, "AHU Watts$"),
-    "Baseboard Heater 1 Watts": (0, 2000, "Baseboard.*Heater.*1.*Watts$"),
-    "Baseboard Heater 2 Watts": (0, 2000, "Baseboard.*Heater.*2.*Watts$"),
-    "Baseboard Heater 3 Watts": (0, 2000, "Baseboard.*Heater.*3.*Watts$"),
-    "Bedroom Plugs Watts": (0, 3000, "Bedroom.?Plugs.*Watts$"),
-    "Cold Water Avg C": (0.0001, 25, "Cold.*Water.*Avg.*C$"),
-    "Cold Water Cubic Meter": (0, 25, "Cold.*Water.*Cubic.*Meter$"),
-    "Dishwasher Watts": (0, 1200, "Dishwasher.*Watts$"),
-    "Dryer (1) Watts": (0, 3500, "Dryer.*1.*Watts$"),
-    "Dryer (2) Watts": (0, 3500, "Dryer.*2.*Watts$"),
-    "Electrical Baseboard 1 Watts": (0, 2000, "Electrical.*Baseboard1.*Watts$"),
-    "Electrical Baseboard 2 Watts": (0, 2000, "Electrical.*Baseboard2.*Watts$"),
-    "Electrical Baseboard 3 Watts": (0, 2000, "Electrical.*Baseboard3.*Watts$"),
-    "Electrical Baseboard 4 Watts": (0, 2000, "Electrical.*Baseboard4.*Watts$"),
-    "Fridge Watts": (0, 627.2, "Fridge.*Watts$"),
-    "Ground Level Plugs Watts": (0, 3000, "Ground.*Level.*Plugs?.*Watts$"),
-    "HRV Watts": (0, 400, "HRV.*Watts$"),
-    "Heat Recovery Water Avg C": (0, 35, "Heat.*Recovery.*Water.*Avg.*C$"),
-    "Hot Water Avg C": (0.0001, 65, "Hot.*Water.*Avg.*C$"),
-    "Hot Water Cubic Meter": (0, 25, "Hot.*Water.*Cubic.*Meter$"),
-    "Hot Water Tank 1 Watts": (0, 4500, "Hot.*Water.*Tank.*1.*Watts$"),
-    "Hot Water Tank 2 Watts": (0, 4500, "Hot.*Water.*Tank.*2.*Watts$"),
-    "Kitchen Counter Plugs Watts": (0, 3000, "Kitchen.*Counter.*Plug?.*Watts$"),
-    "Living Room Plugs Watts": (0, 3000, "Living.*Room.*Plugs?.*Watts$"),
-    "Main Electricity 1 Watts": (0, 10000, "Main.*Electricity.*1.*Watts$"),
-    "Main Electricity 2 Watts": (0, 10000, "Main.*Electricity.*2.*Watts$"),
-    "Main Electricity Gen Watts": (-1500, 10000, "Main.*Electricity.*Gen.*Watts$"),
-    "Main Electricity Gen Watts 1": (-1500, 10000, "Main.*Electricity.*Gen.*Watts.*1$"),
-    "Main Floor Plugs Watts": (0, 3000, "Main.*Floor.*Plugs?.*Watts$"),
-    "Natural Gas": (0, 25, "Natural.*Gas"),
-    "Office Room Plugs Watts": (0, 3000, "Office.*Room.*Plugs?.*Watts$"),
-    "PV Generation 1 Watts": (0, 1000, "PV.*Generation.*1.*Watts$"),
-    "PV Generation 2 Watts": (0, 1000, "PV.*Generation.*2.*Watts$"),
-    "Range (1) Watts": (0, 11300, "Range.*1.*Watts$"),
-    "Range (2) Watts": (0, 11300, "Range.*2.*Watts$"),
-    "Return Air Avg C": (0, 35, "Return.*Air.*Avg.*C$"),
-    "Second Floor Plugs Watts": (0, 3000, "Second.*Floor.*Plugs?.*Watts$"),
-    "Tankless WaterHeater Watts": (0, 250, "Tankless.*WaterHeater.*Watts$"),
-    "Volts": (60, 180, "Volts"),
-    "Washing Machine Watts": (0, 1000, "Washing.*Machine.*Watts$")
+    "A/C Watts": Channel("A/C Watts", 0, 3000, "A/C Watts$", check_limits),
+    "AHU Watts": Channel("AHU Watts", 0, 310, "AHU Watts$", check_limits),
+    "Baseboard Heater 1 Watts": Channel("Baseboard Heater 1 Watts", 0, 2000, "Baseboard.*Heater.*1.*Watts$", check_limits),
+    "Baseboard Heater 2 Watts": Channel("Baseboard Heater 2 Watts", 0, 2000, "Baseboard.*Heater.*2.*Watts$", check_limits),
+    "Baseboard Heater 3 Watts": Channel("Baseboard Heater 3 Watts", 0, 2000, "Baseboard.*Heater.*3.*Watts$", check_limits),
+    "Bedroom Plugs Watts": Channel("Bedroom Plugs Watts", 0, 3000, "Bedroom.?Plugs.*Watts$", check_limits),
+    "Dishwasher Watts": Channel("Dishwasher Watts", 0, 1200, "Dishwasher.*Watts$", check_limits),
+    "Dryer (1) Watts": Channel("Dryer (1) Watts", 0, 3500, "Dryer.*1.*Watts$", check_limits),
+    "Dryer (2) Watts": Channel("Dryer (2) Watts", 0, 3500, "Dryer.*2.*Watts$", check_limits),
+    "Electrical Baseboard 1 Watts": Channel("Electrical Baseboard 1 Watts", 0, 2000, "Electrical.*Baseboard1.*Watts$", check_limits),
+    "Electrical Baseboard 2 Watts": Channel("Electrical Baseboard 2 Watts", 0, 2000, "Electrical.*Baseboard2.*Watts$", check_limits),
+    "Electrical Baseboard 3 Watts": Channel("Electrical Baseboard 3 Watts", 0, 2000, "Electrical.*Baseboard3.*Watts$", check_limits),
+    "Electrical Baseboard 4 Watts": Channel("Electrical Baseboard 4 Watts", 0, 2000, "Electrical.*Baseboard4.*Watts$", check_limits),
+    "Fridge Watts": Channel("Fridge Watts", 0, 627.2, "Fridge.*Watts$", check_limits),
+    "Ground Level Plugs Watts": Channel("Ground Level Plugs Watts", 0, 3000, "Ground.*Level.*Plugs?.*Watts$", check_limits),
+    "HRV Watts": Channel("HRV Watts", 0, 400, "HRV.*Watts$", check_limits),
+    "Hot Water Tank 1 Watts": Channel("Hot Water Tank 1 Watts", 0, 4500, "Hot.*Water.*Tank.*1.*Watts$", check_limits),
+    "Hot Water Tank 2 Watts": Channel("Hot Water Tank 2 Watts", 0, 4500, "Hot.*Water.*Tank.*2.*Watts$", check_limits),
+    "Kitchen Counter Plugs Watts": Channel("Kitchen Counter Plugs Watts", 0, 3000, "Kitchen.*Counter.*Plug?.*Watts$", check_limits),
+    "Living Room Plugs Watts": Channel("Living Room Plugs Watts", 0, 3000, "Living.*Room.*Plugs?.*Watts$", check_limits),
+    "Main Electricity 1 Watts": Channel("Main Electricity 1 Watts", 0, 10000, "Main.*Electricity.*1.*Watts$", check_limits),
+    "Main Electricity 2 Watts": Channel("Main Electricity 2 Watts", 0, 10000, "Main.*Electricity.*2.*Watts$", check_limits),
+    "Main Electricity Gen Watts": Channel("Main Electricity Gen Watts", -1500, 10000, "Main.*Electricity.*Gen.*Watts$", check_limits),
+    "Main Electricity Gen Watts 1": Channel("Main Electricity Gen Watts 1", -1500, 10000, "Main.*Electricity.*Gen.*Watts.*1$", check_limits),
+    "Main Floor Plugs Watts": Channel("Main Floor Plugs Watts", 0, 3000, "Main.*Floor.*Plugs?.*Watts$", check_limits),
+    "Office Room Plugs Watts": Channel("Office Room Plugs Watts", 0, 3000, "Office.*Room.*Plugs?.*Watts$", check_limits),
+    "PV Generation 1 Watts": Channel("PV Generation 1 Watts", 0, 1000, "PV.*Generation.*1.*Watts$", check_limits),
+    "PV Generation 2 Watts": Channel("PV Generation 2 Watts", 0, 1000, "PV.*Generation.*2.*Watts$", check_limits),
+    "Range (1) Watts": Channel("Range (1) Watts", 0, 11300, "Range.*1.*Watts$", check_limits),
+    "Range (2) Watts": Channel("Range (2) Watts", 0, 11300, "Range.*2.*Watts$", check_limits),
+    "Second Floor Plugs Watts": Channel("Second Floor Plugs Watts", 0, 3000, "Second.*Floor.*Plugs?.*Watts$", check_limits),
+    "Tankless WaterHeater Watts": Channel("Tankless WaterHeater Watts", 0, 250, "Tankless.*WaterHeater.*Watts$", check_limits),
+    "Washing Machine Watts": Channel("Washing Machine Watts", 0, 1000, "Washing.*Machine.*Watts$", check_limits),
+    "Return Air Avg C": Channel("Return Air Avg C", 0, 35, "Return.*Air.*Avg.*C$", check_temperature),
+    "Cold Water Avg C": Channel("Cold Water Avg C", 0.0001, 25, "Cold.*Water.*Avg.*C$", check_temperature),
+    "Heat Recovery Water Avg C": Channel("Heat Recovery Water Avg C", 0, 35, "Heat.*Recovery.*Water.*Avg.*C$", check_temperature),
+    "Hot Water Avg C": Channel("Hot Water Avg C", 0.0001, 65, "Hot.*Water.*Avg.*C$", check_temperature),
+    "Volts": Channel("Volts", 60, 180, "Volts", check_limits),
+    "Cold Water Cubic Meter": Channel("Cold Water Cubic Meter", 0, 25, "Cold.*Water.*Cubic.*Meter$", check_limits),
+    "Hot Water Cubic Meter": Channel("Hot Water Cubic Meter", 0, 25, "Hot.*Water.*Cubic.*Meter$", check_limits),
+    "Natural Gas": Channel("Natural Gas", 0, 25, "Natural.*Gas", check_limits)
 }
