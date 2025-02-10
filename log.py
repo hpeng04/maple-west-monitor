13befd98c4fa6a49e8f98b35478de4534b8dcb04
hpeng04 <144637938+hpeng04@users.noreply.github.com>
2025-02-10 16:10:09 -0700

    add dir existence check

--- log.py
+++ log.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
+import os
 
 class Log:
     path = r'Logs/'
 
     @staticmethod
     def write(message):
+        if not os.path.exists(Log.path):
+            os.makedirs(Log.path)
         yesterday = datetime.now() - timedelta(days=1)
         formatted_date = yesterday.strftime('%Y-%m-%d')
         path = Log.path + formatted_date + '.txt'
