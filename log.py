5cc80dac96cb2f5b93e494f2b79455ee02b77878
hpeng04 <144637938+hpeng04@users.noreply.github.com>
2025-02-10 15:30:55 -0700

    logs now stored in ./Logs/yyyy-mm-dd.txt

--- log.py
+++ log.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta
+
 class Log:
-    path = 'log.txt'
+    path = r'Logs/'
 
     @staticmethod
     def write(message):
-        
-        with open(Log.path, 'a') as file:
+        yesterday = datetime.now() - timedelta(days=1)
+        formatted_date = yesterday.strftime('%Y-%m-%d')
+        path = Log.path + formatted_date + '.txt'
+        with open(path, 'a') as file:
             file.write(message + '\n')
