9fe729e180dce607a836c921155a6e3b722678a9
hpeng04 <144637938+hpeng04@users.noreply.github.com>
2025-02-10 17:44:52 -0700

    add path getter

--- log.py
+++ log.py
@@ -13,3 +13,10 @@ class Log:
         path = Log.path + formatted_date + '.txt'
         with open(path, 'a') as file:
             file.write(message + '\n')
+    
+    @staticmethod
+    def get_path():
+        yesterday = datetime.now() - timedelta(days=1)
+        formatted_date = yesterday.strftime('%Y-%m-%d')
+        path = Log.path + formatted_date + '.txt'
+        return path
