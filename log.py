57eac1d3b3ce8bb0fd14c494bc49f657c62b3921
hpeng04 <144637938+hpeng04@users.noreply.github.com>
2025-02-11 22:45:38 -0700

    add log.txt if is_loaded=True

--- log.py
+++ log.py
@@ -5,12 +5,15 @@ class Log:
     path = r'Logs/'
 
     @staticmethod
-    def write(message):
+    def write(message, is_loaded=False):
         if not os.path.exists(Log.path):
             os.makedirs(Log.path)
-        yesterday = datetime.now() - timedelta(days=1)
-        formatted_date = yesterday.strftime('%Y-%m-%d')
-        path = Log.path + formatted_date + '.txt'
+        if is_loaded == False:
+            yesterday = datetime.now() - timedelta(days=1)
+            formatted_date = yesterday.strftime('%Y-%m-%d')
+            path = Log.path + formatted_date + '.txt'
+        else:
+            path = Log.path + 'log.txt'
         with open(path, 'a') as file:
             file.write(message + '\n')
     
