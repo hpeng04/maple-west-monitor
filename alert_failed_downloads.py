101e056ddf81d097c5121a014bbe2f268ebd57a5
hpeng04 <144637938+hpeng04@users.noreply.github.com>
2025-02-21 11:09:24 -0700

    add failed downloads alert

---
+++ alert_failed_downloads.py
@@ -0,0 +1,13 @@
+from alert import send_email
+
+def alert_failed_downloads(path):
+    with open(path, 'r') as f:
+        subject = 'Maple West Failed Downloads'
+        body = f.read()
+        send_email(subject, body, attachment=path)
+    with open(path, 'w') as f:
+        f.truncate(0)
+    return
+
+if __name__ == '__main__':
+    alert_failed_downloads('failed_downloads.txt')
